b923ff82adf238ed0f7930246870ee1cb00d4bab
Bruno Pozzebon <bruno.pozzebon@cwi.com.br>
2020-05-10 22:32:48 -0300

    It's Aliveeeee

---
+++ build/precache-manifest.e786e814c675219683a34d1f4ec99214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab217db29e9640349d1c554cc164dc96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27cc5f3363d2a2431ad8",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f891ab6d57d2a7245f85",
+    "url": "/static/js/2.f53c4c3d.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/static/js/2.f53c4c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27cc5f3363d2a2431ad8",
+    "url": "/static/js/main.6aa1211d.chunk.js"
+  },
+  {
+    "revision": "e663df2881e39a90859c",
+    "url": "/static/js/runtime-main.225c0f6f.js"
+  },
+  {
+    "revision": "1014122de029e2f44a37187a63efff87",
+    "url": "/static/media/logo.1014122d.png"
+  }
+]);
